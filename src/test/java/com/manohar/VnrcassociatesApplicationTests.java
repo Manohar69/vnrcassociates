daccf57b44868f5281e062917518377eeaf6416b
= <=>
2022-03-18 14:49:08 -0400

    changes

--- src/test/java/com/manohar/VnrcassociatesApplicationTests.java
+++ src/test/java/com/manohar/VnrcassociatesApplicationTests.java
@@ -1,13 +1,12 @@
-package com.manohar;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class VnrcassociatesApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+/*
+ * package com.manohar;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class VnrcassociatesApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
